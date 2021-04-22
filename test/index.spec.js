7a6ce16d51fd0e393b564e5d15e692a0cab82579
Goo <kangih90@gmail.com>
2021-04-22 23:55:38 +0900

    add testing framework

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+var assert = require('chai').assert;
 const calendar = require('../src/index.js');
 
 describe('index.js!', function(){
@@ -6,39 +6,39 @@ describe('index.js!', function(){
     context('function check(#1, #2)', function(){
 
         it('param #2 was not empty.', function(){
-            assert.strictEqual(calendar.check(1), false);
+            assert.equal(calendar.check(1), false);
         });
 
         it('param #2 greater then 0.', function(){
-            assert.strictEqual(calendar.check(1, -1), false);
+            assert.equal(calendar.check(1, -1), false);
         });
 
         it('param #2 greater then 0.', function(){
-            assert.strictEqual(calendar.check(1, 0), false);
+            assert.equal(calendar.check(1, 0), false);
         });
 
         it('param #2 greater then 0.', function(){
-            assert.strictEqual(calendar.check(1, '-1'), false);
+            assert.equal(calendar.check(1, '-1'), false);
         });
 
         it('param #2 greater then 0.', function(){
-            assert.strictEqual(calendar.check(1, '0'), false);
+            assert.equal(calendar.check(1, '0'), false);
         });
 
         it('param #1 not false', function(){
-            assert.strictEqual(calendar.check(0, '2'), false);
+            assert.equal(calendar.check(0, '2'), false);
         });
 
         it('param #1 length > #2', function(){
-            assert.strictEqual(calendar.check('100', '2'), '10');
+            assert.equal(calendar.check('100', '2'), '10');
         });
 
         it('param #1 length == #2', function(){
-            assert.strictEqual(calendar.check(10, '2'), '10');
+            assert.equal(calendar.check(10, '2'), '10');
         });
 
         it('param #1 length < #2', function(){
-            assert.strictEqual(calendar.check(10, '3'), '010');
+            assert.equal(calendar.check(10, '3'), '010');
         });
     });
 });
